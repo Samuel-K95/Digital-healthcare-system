afa550c9eac550bbe340bcea23271dd9cc5b10da
Eyob <eyobhg3@gmail.com>
2024-06-15 17:57:19 +0300

    Add homepage and adjust static files

--- Digitalhealthcare/settings.py
+++ Digitalhealthcare/settings.py
@@ -107,7 +107,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# Configure static files
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR /'static'
+STATICFILES_DIRS =[
+    'Digitalhealthcare/static'
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- Digitalhealthcare/views.py
+++ Digitalhealthcare/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'Digitalhealthcare/home.html')
+    return render(request, 'home.html')
