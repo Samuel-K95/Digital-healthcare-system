6726c69980f3fe742a6b3571b90532a8b58fc50d
Eyob <eyobhg3@gmail.com>
2024-06-17 23:58:57 +0300

    Begin adding the doctor class

--- doctors/urls.py
+++ doctors/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="DoctorHome")
+    # path('', views.dashboard, name="dashboard"),
+    # path('DoctorProfile/<int:pk>', views.DoctorProfile, name="DoctorProfile"),
+    # path('DoctorSignUp/', views.DoctorSignUp, name="DoctorSignUp"),
+    # path('DoctorLogin/', views.DoctorLogin, name="DoctorLogin"),
 ]
