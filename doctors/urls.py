21a95da5766a7c06e7d207cbc75acbec64450497
Eyob <eyobhg3@gmail.com>
2024-06-18 23:51:29 +0300

    Add doctor searching and filtering functionalities

--- doctors/urls.py
+++ doctors/urls.py
@@ -5,9 +5,10 @@ from . import views
 urlpatterns = [
     # path('', views.dashboard, name="dashboard"),
     path('DoctorProfile/', views.DoctorProfile, name="DoctorProfile"),
-    # path('EditProfile/<int:pk>', views.DoctorProfile, name="DoctorProfile"),
     path('DoctorSignUp/', views.registerDoctor, name="DoctorSignUp"),
     path('DoctorLogin/', views.DoctorLogin, name="DoctorLogin"),
     path('DoctorLogout/', views.DoctorLogout, name="DoctorLogout"),
+    path('BrowseDoctors/', views.BrowseDoctors, name="BrowseDoctors"),
+    path('DoctorDetail/<int:pk>', views.DoctorDetail, name="DoctorDetail"),
 
 ]
