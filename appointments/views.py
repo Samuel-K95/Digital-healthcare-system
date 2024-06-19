0b648c1485e6cc83a1ac0dd107d7164057831078
Eyob <eyobhg3@gmail.com>
2024-06-19 17:21:46 +0300

    Implement Appontment confirming, rescheduling, deleting, and editing.

--- appointments/views.py
+++ appointments/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import get_object_or_404, render, redirect
 from .models import Appointment
 from doctors.models import Doctor
 from patients.models import Patient
-from .forms import AppointmentForm
+from .forms import AppointmentForm, RescheduleAppointmentForm
 from django.contrib import messages
 
 
@@ -29,7 +29,7 @@ def ScheduleAppointment(request, doctor_id):
                     messages.error(request, f"{error}")
     else:
         form = AppointmentForm()
-    return render(request, 'doctors/schedule_appointment.html', {'form': form, 'doctor': doctor})
+    return render(request, 'patients/schedule_appointment.html', {'form': form, 'doctor': doctor})
 
 
 @login_required
@@ -44,7 +44,7 @@ def ViewMyDoctorAppointments(request):
     patient = get_object_or_404(Patient, user=request.user)
     appointments = Appointment.objects.filter(patient=patient)
 
-    return render(request, 'patients/patient_appointments.html', {'appointments': appointments})
+    return render(request, 'patients/doctor_appointments.html', {'appointments': appointments})
 
 @login_required
 def DeleteAppointment(request, appointment_id):
@@ -53,13 +53,53 @@ def DeleteAppointment(request, appointment_id):
     messages.success(request, "Appointment Deleted Successfully")
     return redirect('view_doctor_appointments')
 
-# @login_required
-# def update_appointment_status(request, appointment_id):
-#     appointment = get_object_or_404(Appointment, id=appointment_id)
-#     if request.method == 'POST':
-#         status = request.POST.get('status')
-#         if status in ['accepted', 'declined', 'rescheduled']:
-#             appointment.status = status
-#             appointment.save()
-#             return redirect('doctor_appointments', appointment.doctor.id)
-#     return render(request, 'update_appointment_status.html', {'appointment': appointment})
+
+@login_required
+def ConfirmAppointment(request, appointment_id):
+
+    appointment = Appointment.objects.get(pk=appointment_id)
+    appointment.status = 'accepted'
+    appointment.save()
+    messages.success(request, "Appointment Accepted Successfully")
+    return redirect('view_patient_appointments')
+
+@login_required
+def RescheduleAppointment(request, appointment_id):
+    appointment = get_object_or_404(Appointment, id=appointment_id)
+    if request.method == 'POST':
+        form = RescheduleAppointmentForm(request.POST)
+        if form.is_valid():
+            appointment.appointment_date = form.cleaned_data['appointment_date']
+            appointment.save()
+            messages.success(request, "Appointment Rescheduled Successfully!")
+            return redirect('view_patient_appointments')
+        else:
+            for field, errors in form.errors.items():
+                for error in errors:
+                    messages.error(request, f"{error}")
+    else:
+        form = RescheduleAppointmentForm()
+    
+    return render(request, 'doctors/reschedule_appointment.html', {'form': form})
+
+@login_required
+def EditAppointment(request, appointment_id):
+    appointment = get_object_or_404(Appointment, id=appointment_id)
+    if request.method == 'POST':
+        form = AppointmentForm(request.POST)
+        if form.is_valid():
+            appointment.appointment_date = form.cleaned_data['appointment_date']
+            appointment.additional_requests = form.cleaned_data['additional_requests']
+            appointment.save()
+            messages.success(request, "Appointment Edited Successfully!")
+            return redirect('view_doctors_appointments')
+        else:
+            for field, errors in form.errors.items():
+                for error in errors:
+                    messages.error(request, f"{error}")
+    else:
+        form = AppointmentForm()
+    
+    return render(request, 'patients/edit_appointment.html', {'form': form})
+
+
