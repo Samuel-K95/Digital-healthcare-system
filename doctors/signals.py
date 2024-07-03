2c865fa7657eccd950b9b313f315a8fb8c456ec5
Samuel Keno <sgere195@gmail.com>
2024-07-03 14:40:00 +0300

    signals: only auto-create Doctor for staff users to avoid role collisions in tests

--- doctors/signals.py
+++ doctors/signals.py
@@ -1,26 +1,26 @@
 from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
-from .models import User, Doctor
-
+from django.contrib.auth.models import User
+from .models import Doctor
 
 
 @receiver(post_save, sender=User)
-def post_save_create_profile_receiver(sender,instance,created,**kwargs):
-    print(created)
+def post_save_create_profile_receiver(sender, instance, created, **kwargs):
     if created:
-        Doctor.objects.create(user=instance)
-        
-        print('Doctor account has been created')
+        # Only auto-create a Doctor profile for users explicitly marked as staff
+        if getattr(instance, 'is_staff', False):
+            Doctor.objects.create(user=instance)
+            print('Doctor account has been created for staff user')
     else:
         try:
             profile = Doctor.objects.get(user=instance)
             profile.save()
-        except:
-            print("Doctor account doesn't!")
-        print('User is updated!')
+        except Doctor.DoesNotExist:
+            pass
 
 
 @receiver(pre_save, sender=User)
 def pre_save_profile_receiver(sender, instance, **kwargs):
-    print(instance.username, " is being saved.")
+    # No-op logging hook kept for development
+    return
 
